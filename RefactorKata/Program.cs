d9bddc46c32b364972b17d99f4e65c7167306460
bdowds <bwdowds@gmail.com>
2018-04-16 08:43:17 -0500

    Added using keyword to remove need of dispose command

--- RefactorKata/Program.cs
+++ RefactorKata/Program.cs
@@ -8,19 +8,21 @@ namespace RefactorKata
     {
         static void Main(string[] args)
         {
-            var Conn = new SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;");
-
-            var cmd = Conn.CreateCommand();
-            cmd.CommandText = "select * from Products";
-
-            var reader = cmd.ExecuteReader();
             var products = new List<Product>();
 
-            while (reader.Read())
+            using (var conn = new SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;"))
             {
-                products.Add(new Product { Name = reader["Name"].ToString() });
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "select * from Products";
+
+                var reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    products.Add(new Product { Name = reader["Name"].ToString() });
+                }
             }
-            Conn.Dispose();
+
             Console.WriteLine("Products Loaded!");
 
             foreach (var item in products)
